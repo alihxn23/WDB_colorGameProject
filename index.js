bf6581b3078b1c3502e818af3097560a1a72e27b
Muhammad Ali Hasan <alihasan9923@gmail.com>
2020-08-19 00:00:08 -0500

    pt7

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ resetButton.addEventListener("click", function(){
 		squares[i].style.background = colors[i];
 	}
 	// reset the color of h1
-	h1.style.background = "#232323";
+	h1.style.background = "steelblue";
 	// update messageDisplay
 	messageDisplay.textContent = "";
 	// update resetButton 
